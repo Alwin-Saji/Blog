ff2bc5253c4d2871a195ed18c084d071cb886b2d
Alwin Saji <alwinsaji4.cgnr@gmail.com>
2026-01-29 18:39:44 +0530

    k

---
+++ Front-end/Front-end/Front-end/src/Components/PostListItem.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import ImageComponent from "./Image.jsx";
+import { Link, useParams } from "react-router-dom";
+import {format} from "timeago.js";
+import { use } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+function PostListItem({post}) {
+
+
+    return(
+        <>
+         <div className="px-10 mb-10 flex flex-col xl:flex-row gap-8">
+         {/* image  */}
+         {post.img && (<div className="md:hidden xl:w-1/3 xl:block ">
+            <ImageComponent src={post.img} className="rounded-3xl object-cover" w="535" />
+         </div>)}
+        {/* details  */}
+       <div className="flex flex-col gap-4 xl:w-2/3">
+        <Link className="text-3xl" to={`/post/${post.slug}`}>{post.title}</Link>
+        <div className="flex items-center gap-4 text-gray-400 text-xs">
+            <span>Written by</span>
+            {post.user && <Link to={`/postlistpage?author=${post.user.username}`} className="text-blue-500 hover:underline">{post.user.username}</Link>}
+            <span>on</span>
+            <Link className="text-blue-500 hover:underline" to={`/postlistpage?cat=${post.category}`}>{post.category}</Link>
+            <span>•</span>
+            <span>{format(post.createdAt)}</span>
+        </div>
+        <p>{post.desc}</p>
+         <Link className="text-sm underline text-blue-200 hover:text-blue-600" to={`/post/${post.slug}`}>Read More</Link>
+  </div>
+       </div>
+        </>
+    )
+}
+export default PostListItem;
---
+++ Front-end/Front-end/Front-end/src/Pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+    return (
+        <>
+        <div className='text-white  '>
+
+        <h1>About Us</h1>
+        <p>Welcome to the About page!</p>
+        </div>
+        </>
+    )
+}
+export default About;

2536d01917fa17cd63d1a7a61cc01980e8ca8746
Alwin Saji <alwinsaji4.cgnr@gmail.com>
2026-01-30 12:53:11 +0530

    feature

--- src/Components/FeaturedPost.jsx
+++ src/Components/FeaturedPost.jsx
@@ -47,7 +47,8 @@ function FeaturedPost() {
               <span>{format(posts[0].createdAt)}</span>
               </div>
             {/* title */}
-            <Link to={posts[0].slug} className="text-lg" >{posts[0].title}</Link>
+            <Link to={`/posts/${posts[0].slug}`} className="text-lg" >{posts[0].title}</Link>
+            <p>{posts[0].desc.length > 150 ? posts[0].desc.substring(0, 150) + "..." : posts[0].desc}</p>
             </div>
              {/* others */}
              <div className="w-full lg:w-1/2 flex flex-col gap-8 px-4">
@@ -66,7 +67,8 @@ function FeaturedPost() {
                 <span>{format(posts[1].createdAt)}</span>
              </div>
              {/* titles */}
-             <Link to={posts[1].slug}>{posts[1].title}</Link>
+             <Link to={`/posts/${posts[1].slug}`}>{posts[1].title}</Link>
+             <p>{posts[1].desc.length > 50 ? posts[1].desc.substring(0, 150) + "..." : posts[1].desc}</p>
              </div>
              </div>
 }
@@ -84,7 +86,8 @@ function FeaturedPost() {
                 <span>{format(posts[2].createdAt)}</span>
              </div>
              {/* titles */}
-             <Link to={posts[2].slug}>{posts[2].title}</Link>
+             <Link to={`/posts/${posts[2].slug}`}>{posts[2].title}</Link>
+             <p>{posts[2].desc.length > 50 ? posts[2].desc.substring(0, 150) + "..." : posts[2].desc}</p>
              </div>
              </div>
 }
@@ -103,7 +106,8 @@ function FeaturedPost() {
                 <span>{format(posts[3].createdAt)}</span>
              </div>
              {/* titles */}
-             <Link to={posts[3].slug}>{posts[3].title}</Link>
+             <Link to={`/posts/${posts[3].slug}`}>{posts[3].title}</Link>
+             <p>{posts[3].desc.length > 50 ? posts[3].desc.substring(0, 150) + "..." : posts[3].desc}</p>
              </div>
              </div>
              }

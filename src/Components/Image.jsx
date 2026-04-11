625bd3db689b8978df8bb65cdb1d041f3d72f948
Alwin Saji <alwinsaji4.cgnr@gmail.com>
2026-04-11 19:09:38 +0530

    v1

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -2,24 +2,36 @@ import { IKImage } from 'imagekitio-react';
 
 
 function ImageComponent({ className, src, h, w, alt }) {
-  
+  const isWebUrl = src?.startsWith("http");
+
   return (
-     <IKImage
-      urlEndpoint={import.meta.env.VITE_IK_URL_ENDPOINT}
-      path={src}
-      className={className}
-      loading="lazy"
-      lqip={{ active: true, quality: 20 }}
-      alt={alt}
-      width={w}
-      height={h}
-      transformation={[
-        {
-          width: w,
-          height: h,
-        },
-      ]}
-    />
+    isWebUrl ? (
+      <img
+        src={src}
+        className={className}
+        loading="lazy"
+        alt={alt}
+        width={w}
+        height={h}
+      />
+    ) : (
+      <IKImage
+        urlEndpoint={import.meta.env.VITE_IK_URL_ENDPOINT}
+        path={src}
+        className={className}
+        loading="lazy"
+        lqip={{ active: true, quality: 20 }}
+        alt={alt}
+        width={w}
+        height={h}
+        transformation={[
+          {
+            width: w,
+            height: h,
+          },
+        ]}
+      />
+    )
   );
 }
 export default ImageComponent;
